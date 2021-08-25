3f367e291c7c16d36655632108b02812cffa2143
vzarate26 <vzarate26@gmail.com>
2021-08-25 16:36:10 -0400

    add rooftop_detail funciton

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
   path('rooftops/', views.rooftops_index, name='rooftops_index'),
-
+  path('rooftops/<int:rooftop_id>/', views.rooftops_detail, name='rooftops_detail'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -12,3 +12,7 @@ def about(request):
 def rooftops_index(request):
   rooftops = Rooftop.objects.all()
   return render(request, 'rooftops/index.html', { 'rooftops': rooftops })
+
+def rooftops_detail(request, rooftop_id):
+  rooftop = Rooftop.objects.get(id=rooftop_id)
+  return render(request, 'rooftops/detail.html', { 'rooftop': rooftop })
