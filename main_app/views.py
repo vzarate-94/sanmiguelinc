4aa7738a80b0355e841852398da017e639ee68a5
vzarate26 <vzarate26@gmail.com>
2021-08-25 13:05:33 -0400

    add base.html

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+  return HttpResponse('<h1>San Miguel INC</h1>')
+
+def about(request):
+  return render(request, 'about.html')
