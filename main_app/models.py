5b268498ba7b2d83bef39b4013246709966b3aeb
vzarate26 <vzarate26@gmail.com>
2021-08-25 16:17:25 -0400

    add rooftop model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Rooftop(models.Model):
+  name = models.CharField(max_length=100)
+  description = models.TextField(max_length=250)
+  price = models.IntegerField()
+
+  def __str__(self):
+    return self.name
--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.shortcuts import render
+from .models import Rooftop
 
 # Create your views here.
 def home(request):
-  return HttpResponse('<h1>San Miguel INC</h1>')
+  return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
+
+
+def rooftops_index(request):
+  rooftops = Rooftop.objects.all()
+  return render(request, 'rooftops/index.html', { 'rooftops': rooftops })
