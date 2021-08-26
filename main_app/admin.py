56b8234d2ab1c2ea8f13075cbe64d6edaa2d00f9
vzarate26 <vzarate26@gmail.com>
2021-08-26 16:23:14 -0400

    register photo model to admin.py

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Rooftop
+from .models import Rooftop, Photo
 
 # Register your models here.
 admin.site.register(Rooftop)
+admin.site.register(Photo)
