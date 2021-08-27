0afe31efb3255b23c0b06bfcfd6a0d8fd7d4eca7
vzarate26 <vzarate26@gmail.com>
2021-08-26 20:52:39 -0400

    import User from django

--- main_app/models.py
+++ main_app/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Rooftop(models.Model):
   name = models.CharField(max_length=100)
   description = models.TextField(max_length=250)
   price = models.IntegerField()
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
 
   def __str__(self):
     return self.name
