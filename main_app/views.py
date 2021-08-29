93ed786ed689a31c9f17313197d3cd4a96064b9c
vzarate26 <vzarate26@gmail.com>
2021-08-28 21:54:30 -0400

    import login_required to main_app/views

--- main_app/views.py
+++ main_app/views.py
@@ -3,6 +3,7 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth.views import LoginView
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
 from .models import Rooftop, Photo
 import uuid
 import boto3
